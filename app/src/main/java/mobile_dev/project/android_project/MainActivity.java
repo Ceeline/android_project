6f6fea50504d55542b34e8536c89978dcdc5c453
Célia <celia.bunouf@efrei.net>
2020-02-25 15:37:17 +0100

    Toolbar added

--- app/src/main/java/mobile_dev/project/android_project/MainActivity.java
+++ app/src/main/java/mobile_dev/project/android_project/MainActivity.java
@@ -1,6 +1,7 @@
 package mobile_dev.project.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
